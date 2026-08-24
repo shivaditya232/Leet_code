fe69e99a7a5154e72459b92dec0f2b97397ababa
shivaditya232 <shivaditya2503@gmail.com>
2026-08-24 12:47:14 +0530

    rotated sorted array 2

---
+++ Search_in_rotated_sorted_array1_medium/main.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int main(){
+    vector<int> nums={5,1,3};
+    vector<int> list={6,1,2,3,4,5};
+    vector<int> list2={1,2,3,4,5,6};
+    int low=0;
+    int high=nums.size()-1;
+    int shift;
+    int target=5;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[low]>nums[mid]){
+            high=mid-1;
+        }
+        else if(nums[high]<nums[mid]){
+            low=mid+1;
+        }
+        else{
+            shift=low;
+            break;
+        }
+    }
+    cout<<shift;
+    low=0;
+    high=nums.size()-1;
+    if(shift!=0){
+        if(nums[0]<=target && nums[shift-1]>=target){
+            high=shift-1;
+
+        }
+        else{
+        low=shift;
+    }
+    }
+    
+    int ans=-1;
+    while(low<=high){
+        int mid1=(low+high)/2;
+        if(nums[mid1]==target){
+            ans=mid1;
+            break;
+        }
+        else if(nums[mid1]<=target){
+            low=mid1+1;
+        }
+        else{
+            high=mid1-1;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
