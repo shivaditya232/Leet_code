00dd319d2be13c88513662fb3954bfb09ab5c415
shivaditya232 <shivaditya2503@gmail.com>
2026-08-29 19:12:16 +0530

    1 problem solved

---
+++ Kth_missing_positive_number_easy/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    vector<int> arr={2,3,4,7,11};
+    int k=5;
+    /*2,3,4,7,11*/
+    /*0,1,2,3,4*/
+    int low=0;
+    int high=arr.size()-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        int ele=arr[mid];
+        int missingEle=ele-(mid+1);
+        if(missingEle>k){
+            high=mid+1;
+        }
+        else{
+            low=mid-1;
+        }
+    }
+    return k-(arr[high]-(high+1))+arr[high];
+    return 0;
+}
